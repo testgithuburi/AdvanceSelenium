3a2b3600ef442f1479f9ef6d648b17bd442b411b
testgithuburi <sakshi.rautela12@gmail.com>
2023-02-07 17:26:18 +0530

    1st commit

---
+++ DummyPro/src/test/java/Practice/SampleTest.java
@@ -0,0 +1,29 @@
+package Practice;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleTest {
+	@Test(groups = {"smoke"})
+	private void ml() {
+		System.out.println("Hello world");
+		//push
+		
+
+	}
+		
+		
+			   
+			
+	}	
+		
+		
+	
+
+
